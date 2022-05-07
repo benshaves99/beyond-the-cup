13df6a3864d7ff33f32b2ec0cf06f74a40f811fc
benshaves99 <102397923+benshaves99@users.noreply.github.com>
2022-05-07 14:05:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 74) {
+		document.getElementById('acidity').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 89) {
+		document.getElementById('aftertaste').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 78) {
+		document.getElementById('aroma').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 68) {
+		document.getElementById('cleancup').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 70) {
+		document.getElementById('cupperpoints').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 71) {
+		document.getElementById('uniformity').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 39) {
+		document.getElementById('flavor').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 40) {
+		document.getElementById('sweetness').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 37) {
+		document.getElementById('body').play();
+	}
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 38) {
+		document.getElementById('balance').play();
+	}
+});
